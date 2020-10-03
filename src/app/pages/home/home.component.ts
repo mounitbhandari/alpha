a4c83fac70c16c47a6e0e3f1f2e59c85e31d79f6
sukanta hui <huisukanta77@gmail.com>
2020-10-03 23:47:59 +0530

    drop down is working

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {DataService, Person} from '../../services/data.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -20,11 +22,16 @@ export class HomeComponent implements OnInit {
   images = [1, 2, 3, 4, 5, 6].map((n) => `assets/carousel/carousel_${n}.jpg`);
   videoUrl = 'https://www.youtube.com/embed/_8kT9xbq5Vk';
   public safeURL: SafeResourceUrl;
-  constructor(private httpClient: HttpClient, private _sanitizer: DomSanitizer) {
+
+  people$: Observable<Person[]>;
+  selectedPersonId = '5a15b13c36e7a7f00cf0d7cb';
+
+  constructor(private httpClient: HttpClient, private _sanitizer: DomSanitizer, private dataService: DataService) {
     this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoUrl);
   }
 
   ngOnInit(){
+    this.people$ = this.dataService.getPeople();
     this.httpClient.get('assets/data.json').subscribe(data => {
       console.log(data);
       this.products = data;
