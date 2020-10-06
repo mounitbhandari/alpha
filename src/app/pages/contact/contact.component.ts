901f99b9ef99128434ea0d5c791d6d869b72fbde
sukanta hui <huisukanta77@gmail.com>
2020-10-06 23:58:19 +0530

    font awsome added

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { faCoffee} from '@fortawesome/free-solid-svg-icons/faCoffee';
+import { faTwitter} from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faPersonBooth } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faAtom} from '@fortawesome/free-solid-svg-icons/faAtom';
 
 @Component({
   selector: 'app-contact',
@@ -8,15 +13,17 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-
+  twitter = faTwitter;
+  person = faPersonBooth;
+  faEnvelope = faEnvelope;
+  faAtom = faAtom;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
-//https://formspree.io
+// https://formspree.io
   onSubmit(contactForm: NgForm) {
-    alert("sending mail");
+    alert('sending mail');
     if (contactForm.valid) {
       const email = contactForm.value;
       const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
