c3d1ba692232c41c300ec733f599d14497ffbfb4
mounitbhandari <tataibhandari@gmail.com>
2020-11-06 15:22:17 +0530

    new commit

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-video',
@@ -8,12 +9,22 @@ import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 })
 export class VideoComponent implements OnInit {
   videoUrl = 'https://www.youtube.com/embed/_8kT9xbq5Vk';
+  videoUrl2 = 'https://www.youtube.com/embed/x6Q7c9RyMzk';
   public safeURL: SafeResourceUrl;
-  constructor( private _sanitizer: DomSanitizer) {
-    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoUrl);
+  safeURLArray: any[] = [];
+
+  videos: any;
+  constructor( private _sanitizer: DomSanitizer, private http: HttpClient) {
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoUrl2);
    }
 
   ngOnInit(): void {
+    this.http.get('assets/video-gallery.json').subscribe((data: any[]) => {
+      this.videos = data;
+      for (const video of this.videos){
+        this.safeURLArray.push(this._sanitizer.bypassSecurityTrustResourceUrl(video.url));
+      }
+    });
   }
 
 }
