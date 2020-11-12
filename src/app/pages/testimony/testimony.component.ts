98787393ddac1d246a22faad808e5f48f599eb48
sukanta hui <huisukanta77@gmail.com>
2020-11-12 09:27:58 +0530

    my commit added

--- src/app/pages/testimony/testimony.component.ts
+++ src/app/pages/testimony/testimony.component.ts
@@ -9,14 +9,17 @@ import {HttpClient} from '@angular/common/http';
 })
 export class TestimonyComponent implements OnInit {
   comments: any ;
-  x = 50;
-  avatar = 'https://live.staticflickr.com/65535/50589371316_538b56a864_o.jpg';
+  sukantahui: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('assets/comments.json').subscribe((data: any[]) => {
       this.comments = data;
       console.log(this.comments);
+    });
+
+    this.http.get('assets/sukantahui.json').subscribe((data: any) => {
+      this.sukantahui = data;
     });
   }
 
