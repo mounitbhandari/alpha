ba2a4754c206af56a55969245d2a6f57d89e0941
sukanta hui <huisukanta77@gmail.com>
2020-11-12 16:22:36 +0530

    contacts updated for mobile

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import {HttpClient} from '@angular/common/http';
+import {CommonService} from '../../services/common.service';
 
 @Component({
   selector: 'app-video',
@@ -11,10 +12,10 @@ export class VideoComponent implements OnInit {
   videoUrl = 'https://www.youtube.com/embed/_8kT9xbq5Vk';
   videoUrl2 = 'https://www.youtube.com/embed/x6Q7c9RyMzk';
   public safeURL: SafeResourceUrl;
-
+  isDeviceXs: boolean;
 
   videos: any;
-  constructor( private _sanitizer: DomSanitizer, private http: HttpClient) {
+  constructor( private _sanitizer: DomSanitizer, private http: HttpClient, public common: CommonService) {
     this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoUrl2);
    }
 
@@ -25,7 +26,7 @@ export class VideoComponent implements OnInit {
         video.safeUrl = this._sanitizer.bypassSecurityTrustResourceUrl(video.url);
         // this.safeURLArray.push(this._sanitizer.bypassSecurityTrustResourceUrl(video.url));
       }
-      console.log(this.videos);
+      this.isDeviceXs = this.common.isDeviceXs;
     });
   }
 
