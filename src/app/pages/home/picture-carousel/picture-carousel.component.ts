e6d41627a4c2d9a9050f77b2328482f18f6b4a9a
mounitbhandari <tataibhandari@gmail.com>
2020-11-13 21:58:47 +0530

    design home

--- src/app/pages/home/picture-carousel/picture-carousel.component.ts
+++ src/app/pages/home/picture-carousel/picture-carousel.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import {Picture} from '../../../models/picture.model';
 import {HttpClient} from '@angular/common/http';
+import {CommonService} from "../../../services/common.service";
 @Component({
   selector: 'app-picture-carousel',
   templateUrl: './picture-carousel.component.html',
@@ -18,9 +19,10 @@ export class PictureCarouselComponent implements OnInit{
   unpauseOnArrow = false;
   pauseOnIndicator = false;
   pauseOnHover = false;
+  isDeviceXs: boolean;
 
   @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public common: CommonService) { }
   togglePaused() {
     if (this.paused) {
       this.carousel.cycle();
@@ -34,6 +36,7 @@ export class PictureCarouselComponent implements OnInit{
       this.pictures = data;
       console.log(this.pictures);
     });
+    this.isDeviceXs = this.common.isDeviceXs;
   }
   onSlide(slideEvent: NgbSlideEvent) {
     if (this.unpauseOnArrow && slideEvent.paused &&
